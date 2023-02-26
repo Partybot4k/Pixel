01e265e226e70e12eca1b672291c723d0e732c3d
Brandon Davis <davisbrandon02@gmail.com>
2023-02-25 20:06:41 -0600

    fixed merge conflicts

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -4,11 +4,8 @@ extends CharacterBody2D
 const SPEED = 20000
 
 func _ready():
-<<<<<<< HEAD
 	get_parent().get_child_count()
-=======
 	pass
->>>>>>> 05adb4471a4e53a717f5d657e0e49e5467123be2
 
 func _physics_process(delta):
 	var x_dir: float = Input.get_axis("left", "right")
@@ -16,8 +13,4 @@ func _physics_process(delta):
 	
 	velocity = Vector2(x_dir, y_dir) * SPEED * delta
 	
-<<<<<<< HEAD
-	
-=======
->>>>>>> 05adb4471a4e53a717f5d657e0e49e5467123be2
 	move_and_slide()
