e5f6d4283a757bda83dbb5e43e8e23e1f040d7cd
Brandon Davis <davisbrandon02@gmail.com>
2023-03-04 11:32:56 -0600

    made shit work

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -5,7 +5,7 @@ const SPEED = 20000
 
 func _ready():
 	get_parent().get_child_count()
-	pass
+	$Pixel.get_node("Sprite2D").modulate = Color(randf_range(0,1),randf_range(0,1),randf_range(0,1))
 
 func _physics_process(delta):
 	var x_dir: float = Input.get_axis("left", "right")
@@ -14,3 +14,18 @@ func _physics_process(delta):
 	velocity = Vector2(x_dir, y_dir) * SPEED * delta
 	
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var singlePixel = get_slide_collision(i).get_collider_shape().get_parent()
+		eatSinglePixel(singlePixel)
+
+var pixelNode = preload("res://pixel.tscn")
+func eatSinglePixel(singlePixel: StaticBody2D):
+	var newPixel = pixelNode.instantiate()
+	var pos_offset = singlePixel.position - position
+	newPixel.position = pos_offset
+	var spSprite: Sprite2D = singlePixel.get_node("CollisionShape2D/Sprite2D")
+	var newSprite: Sprite2D = newPixel.get_node("Sprite2D")
+	newSprite.modulate = spSprite.modulate
+	singlePixel.queue_free()
+	add_child(newPixel)
--- pixel.gd
+++ pixel.gd
@@ -1,12 +1,5 @@
-class_name Pixel
-extends Node2D
+class_name SinglePixel
+extends StaticBody2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	$CollisionShape2D/Sprite2D.modulate = Color(randf_range(0,1),randf_range(0,1),randf_range(0,1))
